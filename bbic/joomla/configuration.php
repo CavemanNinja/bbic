3938ddd591c08f495110b361eea5aec182fe280b
Jalal Albasri <jalal.albasri@gmail.com>
2015-03-18 09:24:59 +0300

    ssl settings

--- bbic/joomla/configuration.php
+++ bbic/joomla/configuration.php
@@ -17,7 +17,6 @@ class JConfig {
 	public $password = '3f32f546';
 	public $db = 'heroku_e299eddf77acc03';
 	public $dbprefix = 'yzhv6_';
-	public $live_site = 'https://afternoon-falls-9745.herokuapp.com';
 	public $secret = 'hNZRfrd97rlRJoor';
 	public $gzip = '0';
 	public $error_reporting = 'none';
@@ -73,7 +72,6 @@ class JConfig {
 	public $proxy_pass = '';
 	public $MetaRights = '';
 	public $sitename_pagetitles = '0';
-	public $force_ssl = '2';
 	public $session_memcache_server_host = 'localhost';
 	public $session_memcache_server_port = '11211';
 	public $session_memcached_server_host = 'localhost';
@@ -83,4 +81,7 @@ class JConfig {
 	public $cookie_domain = '';
 	public $cookie_path = '';
 	public $asset_id = '1';
+
+	public $force_ssl = '0';
+	public $live_site = 'https://afternoon-falls-9745.herokuapp.com';
 }
