dd6b6209ef98c139b70bb157a412f6f4ce4adf3d
Jalal Albasri <jalal.albasri@gmail.com>
2015-03-19 14:02:50 +0300

    sef

--- bbic/joomla/templates/t3_bs3_blank/html/mod_login/default.php
+++ bbic/joomla/templates/t3_bs3_blank/html/mod_login/default.php
@@ -39,7 +39,7 @@ if (version_compare(JVERSION, '3.0', 'ge')) {
 		</div>
 	</form>
 <?php else : ?>
-	<form action="<?php echo JRoute::_('https://afternoon-falls-9745.herokuapp.com/bbic/joomla/index.php?lang=en', true, $params->get('usesecure')); ?>" method="post" id="login-form">
+	<form action="<?php echo JRoute::_('index.php', true, $params->get('usesecure')); ?>" method="post" id="login-form">
 		<?php if ($params->get('pretext')): ?>
 			<div class="pretext">
 				<p><?php echo $params->get('pretext'); ?></p>
