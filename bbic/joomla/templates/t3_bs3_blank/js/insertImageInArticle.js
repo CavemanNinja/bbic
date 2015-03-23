f5818a1c03bb9f5a80100afb3b013ca5cb538eae
Jalal Albasri <jalal.albasri@gmail.com>
2015-03-23 17:20:14 +0300

    news images

--- bbic/joomla/templates/t3_bs3_blank/js/insertImageInArticle.js
+++ bbic/joomla/templates/t3_bs3_blank/js/insertImageInArticle.js
@@ -2,6 +2,6 @@ jQuery(function(){
 	console.log('insertImageInArticle');
 	jQuery('#imgInsertButton').on('click', function(){
 		console.log('imgInsertButton: ' + jQuery('#jform_images_image_fulltext').val() );
-		jQuery('#tinymce').prepend('<p><img src="' + jQuery('#jform_images_image_fulltext').val() + '"></p>');
+		jQuery('#tinymce').html('<p><img src="' + jQuery('#jform_images_image_fulltext').val() + '"></p>' + jQuery('#tinymce').html());
 	});	
 });
