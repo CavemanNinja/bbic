2cf80151b652ab0749a036bad885ce01f7ab03c4
Jalal Albasri <jalal.albasri@gmail.com>
2015-03-24 10:23:27 +0300

    news image js

--- bbic/joomla/templates/t3_bs3_blank/js/insertImageInArticle.js
+++ bbic/joomla/templates/t3_bs3_blank/js/insertImageInArticle.js
@@ -1,6 +1,6 @@
 jQuery(function(){
 	console.log('insertImageInArticle');
-	jQuery('#imgInsertButton').on('click', function(){
+	jQuery('#popupInsertImage').on('click', function(){
 		var iframe = jQuery('#jform_articletext_ifr');
 		var tinymce = jQuery('#tinymce', iframe.contents());
 		tinymce.html('<p><img src="' + jQuery('#jform_images_image_fulltext').val() + '"></p>' + tinymce.html());
