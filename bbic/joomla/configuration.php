32cd21e3e1481a1ac9e14fa06d57b12b629a2253
Jalal Albasri <jalal.albasri@gmail.com>
2015-03-24 15:16:33 +0300

    rename app back to joomla

--- bbic/joomla/configuration.php
+++ bbic/joomla/configuration.php
@@ -20,6 +20,7 @@ class JConfig {
 	public $live_site = '';
 	public $secret = 'hNZRfrd97rlRJoor';
 	public $gzip = '0';
+	
 	public $error_reporting = 'none';
 	public $helpurl = 'http://help.joomla.org/proxy/index.php?option=com_help&keyref=Help{major}{minor}:{keyref}';
 	public $ftp_host = '';
