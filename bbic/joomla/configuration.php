7ed51051d465d18a03932df7cb0cff4e73e47863
Jalal Albasri <jalal.albasri@gmail.com>
2015-03-31 12:12:05 +0300

    fixing sql service requests

--- bbic/joomla/configuration.php
+++ bbic/joomla/configuration.php
@@ -12,9 +12,9 @@ class JConfig {
 	public $debug = '0';
 	public $debug_lang = '0';
 	public $dbtype = 'mysqli';
-	public $host = 'us-cdbr-iron-east-01.cleardb.net';
-	public $user = 'b2c8d807e51feb';
-	public $password = '3f32f546';
+public $host = 'localhost';
+public $user = 'root';
+public $password = '';
 	public $db = 'heroku_e299eddf77acc03';
 	public $dbprefix = 'yzhv6_';
 	public $live_site = '';
