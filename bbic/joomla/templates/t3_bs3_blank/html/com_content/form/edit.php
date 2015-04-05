0897fe6d740fba84463b6f38af20ace2a3c4ac27
Jalal Albasri <jalal.albasri@gmail.com>
2015-04-05 15:49:17 +0300

    fixing edit access

--- bbic/joomla/templates/t3_bs3_blank/html/com_content/form/edit.php
+++ bbic/joomla/templates/t3_bs3_blank/html/com_content/form/edit.php
@@ -76,7 +76,7 @@ if ($catid == 10 && in_array(14, $user_groups)) {
 	$authorized = true;
 }
 // Public Pages
-if (($catid == 11 || $catid == 30 || $parentid == 11) && in_array(11, $user_groups)) {
+if (($catid == 11 || $parentid == 11) && in_array(11, $user_groups)) {
 	$authorized = true;
 }
 // Service Requests
@@ -1892,6 +1892,424 @@ if(count($extrafields)){
 				</fieldset>
 			</form>
 		</div>
+	<?php elseif($catid == 11 || $parentid == 11) : ?>
+		<div class="edit item-page<?php echo $this->pageclass_sfx; ?>">
+			<?php if ($params->get('show_page_heading', 1)) : ?>
+			<div class="page-header">
+				<h1>
+					<?php echo $this->escape($params->get('page_heading')); ?>
+				</h1>
+			</div>
+			<?php endif; ?>
 
+			<form action="<?php echo JRoute::_('index.php?option=com_content&a_id='.(int) $this->item->id); ?>" role="form" method="post" name="adminForm" id="adminForm" class="form-validate">
+				<fieldset>
+
+					<ul class="nav nav-tabs">
+						<li class="active"><a href="#editor" data-toggle="tab"><?php echo JText::_('JEDITOR') ?></a></li>
+						<?php if(count($extrafields)) : ?>
+						<li><a href="#extrafields" data-toggle="tab"><?php echo JText::_('T3_EXTRA_FIELDS_GROUP_LABEL') ?></a></li>
+						<?php endif; ?>
+						<?php if ($params->get('show_urls_images_frontend') ) : ?>
+						<li><a href="#images" data-toggle="tab"><?php echo JText::_('COM_CONTENT_IMAGES_AND_URLS') ?></a></li>
+						<?php endif; ?>
+
+					</ul>
+
+					 <input type="hidden" value="<?php echo $user->id ?>" id="usrid" />
+
+					<div class="tab-content">
+						<div class="tab-pane active" id="editor">
+
+							<div class="form-group">
+								<?php echo $this->form->getLabel('title'); ?>
+								<?php echo $this->form->getInput('title'); ?>
+							</div>
+
+							<?php if (is_null($this->item->id)) : ?>
+							<div class="form-group hidden">
+								<?php echo $this->form->getLabel('alias'); ?>
+								<?php echo $this->form->getInput('alias'); ?>
+							</div>
+							<?php endif; ?>
+
+							<div class="form-group">
+								<?php echo $this->form->getInput('articletext'); ?>
+							</div>
+						</div>
+
+						<div class="form-group">
+							<?php echo $this->form->getLabel('catid'); ?>
+							<?php
+
+								$lang = JFactory::getLanguage()->get('tag');
+								// var_dump($lang);
+								if ($lang == "en-GB") {
+									$selectcat = $this->form->getInput('catid');
+									// echo "START".$selectcat."END";
+
+									$selectcat = str_replace('<option value="8" selected="selected">- - News</option>',
+									 '', $selectcat );
+
+									$selectcat = str_replace('<option value="8">- - News</option>',
+									 '', $selectcat );
+
+									$selectcat = str_replace('<option value="9">- - Company Profiles</option>',
+									 '', $selectcat );
+
+									$selectcat = str_replace('<option value="10">- Billing</option>',
+									 '',  $selectcat);
+
+									$selectcat = str_replace('<option value="12">- Service Requests</option>',
+									 '', $selectcat );
+
+									$selectcat = str_replace('<option value="13">- Downloads</option>',
+									 '', $selectcat );
+
+									$selectcat = str_replace('<option value="19">- Services</option>',
+									 '', $selectcat );
+
+									$selectcat = str_replace('<option value="28">- - Company Profiles</option>',
+									 '', $selectcat );
+									$selectcat = str_replace('<option value="22">- - الأخبار</option>',
+									 '', $selectcat );
+									$selectcat = str_replace('<option value="23">- - - أخبار ب ب أ سي </option>',
+									 '', $selectcat );
+									$selectcat = str_replace('<option value="24">- - - أخبار ب د ب</option>',
+									 '', $selectcat );
+									$selectcat = str_replace('<option value="25">- - - أخبار اضافية</option>',
+									 '', $selectcat );
+
+									$selectcat = str_replace('<option value="23">- - - أخبارالمركز </option>', '', $selectcat);
+									$selectcat = str_replace('<option value="24">- - - أخبار بنك للتنمية</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="25">- - - أخبار المستأجرين</option>', '', $selectcat);
+
+									$selectcat = str_replace('<option value="33">- - - Industrial</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="34">- - - Retail</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="50">- - - Food</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="49">- - - Consulting</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="51">- - - Society</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="52">- - - Trading</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="53">- - - Landscaping</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="54">- - - Contracting</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="55">- - - Recreational</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="56">- - - Advertising</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="57">- - - Fabricatiion</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="58">- - - Furniture</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="59">- - - Technology</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="60">- - - Transportation</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="61">- - - Marine</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="62">- - - Cleaning</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="63">- - - Event Management</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="64">- - - Media</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="65">- - - Design</option>', '', $selectcat);
+
+									echo $selectcat;
+
+								} else {
+									$selectcat = $this->form->getInput('catid');
+
+									$selectcat = str_replace('<option value="22" selected="selected">- - الأخبار</option>',
+									 '', $selectcat );
+
+									$selectcat = str_replace('<option value="8" selected="selected">- - News</option>',
+									 '', $selectcat );
+
+									$selectcat = str_replace('<option value="8">- - News</option>',
+									 '', $selectcat );
+
+									$selectcat = str_replace('<option value="9">- - Company Profiles</option>',
+									 '', $selectcat );
+
+									$selectcat = str_replace('<option value="10">- Billing</option>',
+									 '',  $selectcat);
+
+									$selectcat = str_replace('<option value="12">- Service Requests</option>',
+									 '', $selectcat );
+
+									$selectcat = str_replace('<option value="13">- Downloads</option>',
+									 '', $selectcat );
+
+									$selectcat = str_replace('<option value="19">- Services</option>',
+									 '', $selectcat );
+
+									$selectcat = str_replace('<option value="28">- - Company Profiles</option>',
+									 '', $selectcat );
+									$selectcat = str_replace('<option value="8">- - News</option>',
+									 '', $selectcat );
+									$selectcat = str_replace('<option value="14">- - - BBIC</option>',
+									 '', $selectcat );
+									$selectcat = str_replace('<option value="15">- - - BDB</option>',
+									 '', $selectcat );
+									$selectcat = str_replace('<option value="16">- - - Extra</option>',
+									 '', $selectcat );
+									$selectcat = str_replace('<option value="16">- - - Tenant</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="33">- - - Industrial</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="34">- - - Retail</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="50">- - - Food</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="49">- - - Consulting</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="51">- - - Society</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="52">- - - Trading</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="53">- - - Landscaping</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="54">- - - Contracting</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="55">- - - Recreational</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="56">- - - Advertising</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="57">- - - Fabricatiion</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="58">- - - Furniture</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="59">- - - Technology</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="60">- - - Transportation</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="61">- - - Marine</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="62">- - - Cleaning</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="63">- - - Event Management</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="64">- - - Media</option>', '', $selectcat);
+									$selectcat = str_replace('<option value="65">- - - Design</option>', '', $selectcat);
+
+									echo $selectcat;
+								}
+
+							?>
+						</div>
+
+						<!-- <div class="tab-pane hidden" id="language">
+						</div> -->
+						<div class="form-group">
+							<?php echo $this->form->getLabel('language'); ?>
+							<?php
+								$lang = JFactory::getLanguage()->get('tag');
+								if ($lang == "en-GB") {
+									$selectlang = $this->form->getInput('language');
+									$selectlang = str_replace(
+										'<option value="en-GB">English</option>',
+										'<option selected value="en-GB">English</option>',
+										$selectlang);
+									echo $selectlang;
+
+
+								} else {
+									$selectlang = $this->form->getInput('language');
+									$selectlang = str_replace(
+										'<option value="ar-AA">عربي</option>',
+										'<option selected value="ar-AA">عربي</option>',
+										$selectlang);
+									echo $selectlang;
+								}
+							?>
+						</div>
+
+						<?php if(count($extrafields)) : ?>
+						<div class="tab-pane" id="extrafields">
+							<?php foreach ($extrafields as $extraset) : ?>
+								<?php foreach ($this->form->getFieldset($extraset->name) as $field) : ?>
+									<div class="form-group">
+										<div class="control-label">
+											<?php echo $field->label; ?>
+										</div>
+										<div class="controls">
+											<?php echo $field->input; ?>
+										</div>
+									</div>
+								<?php endforeach ?>
+							<?php endforeach ?>
+						</div>
+						<?php endif; ?>
+
+						<div class="form-group">
+							<?php echo $this->form->getLabel('image_fulltext', 'images'); ?>
+							<?php echo $this->form->getInput('image_fulltext', 'images'); ?>
+						</div>
+
+						<?php if ($params->get('show_urls_images_frontend')): ?>
+						<div class="tab-pane" id="images">
+
+							<div class="form-group">
+								<?php echo $this->form->getLabel('image_intro', 'images'); ?>
+								<?php echo $this->form->getInput('image_intro', 'images'); ?>
+							</div>
+
+							<div class="form-group">
+								<?php echo $this->form->getLabel('image_intro_alt', 'images'); ?>
+								<?php echo $this->form->getInput('image_intro_alt', 'images'); ?>
+							</div>
+
+							<div class="form-group">
+								<?php echo $this->form->getLabel('image_intro_caption', 'images'); ?>
+								<?php echo $this->form->getInput('image_intro_caption', 'images'); ?>
+							</div>
+
+							<div class="form-group">
+								<?php echo $this->form->getLabel('float_intro', 'images'); ?>
+								<?php echo $this->form->getInput('float_intro', 'images'); ?>
+							</div>
+
+							<div class="form-group">
+								<?php echo $this->form->getLabel('image_fulltext', 'images'); ?>
+								<?php echo $this->form->getInput('image_fulltext', 'images'); ?>
+							</div>
+
+							<div class="form-group">
+								<?php echo $this->form->getLabel('image_fulltext_alt', 'images'); ?>
+								<?php echo $this->form->getInput('image_fulltext_alt', 'images'); ?>
+							</div>
+
+							<div class="form-group">
+								<?php echo $this->form->getLabel('image_fulltext_caption', 'images'); ?>
+								<?php echo $this->form->getInput('image_fulltext_caption', 'images'); ?>
+							</div>
+
+							<div class="form-group">
+								<?php echo $this->form->getLabel('float_fulltext', 'images'); ?>
+								<?php echo $this->form->getInput('float_fulltext', 'images'); ?>
+							</div>
+
+							<div class="form-group">
+								<?php echo $this->form->getLabel('urla', 'urls'); ?>
+								<?php echo $this->form->getInput('urla', 'urls'); ?>
+							</div>
+
+							<div class="form-group">
+								<?php echo $this->form->getLabel('urlatext', 'urls'); ?>
+								<?php echo $this->form->getInput('urlatext', 'urls'); ?>
+							</div>
+
+							<div class="form-group">
+								<?php echo $this->form->getInput('targeta', 'urls'); ?>
+							</div>
+
+							<div class="form-group">
+								<?php echo $this->form->getLabel('urlb', 'urls'); ?>
+								<?php echo $this->form->getInput('urlb', 'urls'); ?>
+							</div>
+
+							<div class="form-group">
+								<?php echo $this->form->getLabel('urlbtext', 'urls'); ?>
+								<?php echo $this->form->getInput('urlbtext', 'urls'); ?>
+							</div>
+
+							<div class="form-group">
+								<?php echo $this->form->getInput('targetb', 'urls'); ?>
+							</div>
+
+							<div class="form-group">
+								<?php echo $this->form->getLabel('urlc', 'urls'); ?>
+								<?php echo $this->form->getInput('urlc', 'urls'); ?>
+							</div>
+
+							<div class="form-group">
+								<?php echo $this->form->getLabel('urlctext', 'urls'); ?>
+								<?php echo $this->form->getInput('urlctext', 'urls'); ?>
+							</div>
+
+							<div class="form-group">
+								<?php echo $this->form->getInput('targetc', 'urls'); ?>
+							</div>
+
+						</div>
+						<?php endif; ?>
+
+						<div class="tab-pane" id="publishing">
+
+
+							<div class="form-group">
+								<?php echo $this->form->getLabel('tags'); ?>
+								<?php echo str_replace('span12', '', $this->form->getInput('tags')); ?>
+							</div>
+
+							<?php if ($params->get('save_history', 0)) : ?>
+							<div class="form-group hidden">
+								<?php echo $this->form->getLabel('version_note'); ?>
+								<?php echo $this->form->getInput('version_note'); ?>
+							</div>
+							<?php endif; ?>
+
+							<div class="form-group hidden">
+								<?php echo $this->form->getLabel('created_by_alias'); ?>
+								<?php echo $this->form->getInput('created_by_alias'); ?>
+							</div>
+
+							<?php if ($this->item->params->get('access-change')) : ?>
+								<div class="form-group">
+									<?php echo $this->form->getLabel('state'); ?>
+									<?php
+										$statepublished = $this->form->getInput('state');
+
+										$statepublished = str_replace('option value="0"',
+											'option value="0" disabled="disabled"', $statepublished);
+										$statepublished = str_replace('option value="2"',
+											'option value="2" disabled="disabled"', $statepublished);
+										$statepublished = str_replace('option value="-2"',
+											'option value="-2" disabled="disabled"', $statepublished);
+										echo $statepublished;
+									?>
+								</div>
+
+								<div class="form-group">
+									<?php echo $this->form->getLabel('featured'); ?>
+									<?php echo $this->form->getInput('featured'); ?>
+								</div>
+
+								<div class="form-group hidden">
+									<?php echo $this->form->getLabel('publish_up'); ?>
+									<?php echo str_replace('class="btn"', 'class="btn btn-default"', $this->form->getInput('publish_up')); ?>
+								</div>
+
+								<div class="form-group hidden">
+									<?php echo $this->form->getLabel('publish_down'); ?>
+									<?php echo str_replace('class="btn"', 'class="btn btn-default"', $this->form->getInput('publish_down')); ?>
+								</div>
+							<?php endif; ?>
+
+							<div class="form-group hidden">
+								<?php echo $this->form->getLabel('access'); ?>
+								<?php echo $this->form->getInput('access'); ?>
+							</div>
+
+							<?php if (is_null($this->item->id)):?>
+								<div class="form-group">
+									<?php echo JText::_('COM_CONTENT_ORDERING'); ?>
+								</div>
+							<?php endif; ?>
+						</div>
+
+
+
+						<div class="tab-pane" id="metadata">
+							<div class="form-group">
+								<?php echo $this->form->getLabel('metadesc'); ?>
+								<?php echo $this->form->getInput('metadesc'); ?>
+							</div>
+
+							<div class="form-group">
+									<?php echo $this->form->getLabel('metakey'); ?>
+									<?php echo $this->form->getInput('metakey'); ?>
+							</div>
+
+							<input type="hidden" name="task" value="" />
+							<input type="hidden" name="return" value="<?php echo $this->return_page; ?>" />
+							<?php if ($this->params->get('enable_category', 0) == 1) :?>
+							<input type="hidden" name="jform[catid]" value="<?php echo $this->params->get('catid', 1); ?>" />
+							<?php endif; ?>
+						</div>
+					</div>
+					<div class="btn-toolbar">
+						<div class="btn-group">
+							<button type="button" class="btn btn-primary" onclick="Joomla.submitbutton('article.save')">
+								<span class="fa fa-ok"></span>&#160;<?php echo JText::_('JSAVE') ?>
+							</button>
+						</div>
+						<div class="btn-group">
+							<button type="button" class="btn btn-default" onclick="Joomla.submitbutton('article.cancel')">
+								<span class="fa fa-cancel"></span>&#160;<?php echo JText::_('JCANCEL') ?>
+							</button>
+						</div>
+						<?php if ($params->get('save_history', 0)) : ?>
+						<div class="btn-group">
+							<?php echo $this->form->getInput('contenthistory'); ?>
+						</div>
+						<?php endif; ?>
+					</div>
+					<?php echo JHtml::_('form.token'); ?>
+				</fieldset>
+			</form>
+		</div>
 	<?php endif; ?>
 <?php endif; ?>
