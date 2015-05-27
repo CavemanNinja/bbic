fa611294c54827df6f7e818ab7ccafdf7e629379
Jalal Albasri <jalal.albasri@gmail.com>
2015-05-27 10:59:26 +0300

    change default editor to none, problem with tinymce editor loading through insecure script

--- bbic/joomla/configuration.php
+++ bbic/joomla/configuration.php
@@ -5,7 +5,7 @@ class JConfig {
 	public $display_offline_message = '1';
 	public $offline_image = '';
 	public $sitename = 'BBIC';
-	public $editor = 'tinymce';
+	public $editor = 'none';
 	public $captcha = '0';
 	public $list_limit = '100';
 	public $access = '1';
