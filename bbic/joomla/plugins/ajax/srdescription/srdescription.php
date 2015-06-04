540bfd4e64cbe0cd03bac1db077240fbe5021e52
Jalal Albasri <jalal.albasri@gmail.com>
2015-06-04 19:20:44 +0300

    service request description

--- bbic/joomla/plugins/ajax/srdescription/srdescription.php
+++ bbic/joomla/plugins/ajax/srdescription/srdescription.php
@@ -8,19 +8,17 @@ class plgAjaxSrdescription extends JPlugin
 {
 	function onAjaxSrdescription()
 	{
-  //               $db = JFactory::getDbo();
-		// $query = $db->getQuery(true);
-  //               $query->select($db->quoteName('attribs'));
-  //               $query->from($db->quoteName('#__content'));
-  //               $query->where($db->quoteName('id') . " = " . $this->params->get('value', ''));
-  //               $db->setQuery($query);
-  //               $result = $db->loadResult();
+                $db = JFactory::getDbo();
+		$query = $db->getQuery(true);
+                $query->select($db->quoteName('attribs'));
+                $query->from($db->quoteName('#__content'));
+                $query->where($db->quoteName('id') . " = " . $_GET["value"]);
+                $db->setQuery($query);
+                $result = $db->loadResult();
 
-  //               $service_attribs = json_decode($result);
-  //               $service_description = $service_attribs->service_description;
+                $service_attribs = json_decode($result);
+                $service_description = $service_attribs->service_description;
 
-		// return json_encode($service_description);
-                
-                return json_encode($_GET["value"]);
+		return json_encode($service_description);
 	}
 }
